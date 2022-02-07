04562e362bdbb2c5e5916b94456362a81e431a77
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2022-02-07 02:37:20 +0100

    search done

--- src/components/Pages/Marketplace.jsx
+++ src/components/Pages/Marketplace.jsx
@@ -1,43 +1,102 @@
-import * as React from 'react';
+import * as React from "react";
 import nftdata from "../nftdata.js";
-
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { useState,useEffect } from "react";
+import axios from "axios"
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export const Marketplace = () => {
+  const [nftdata, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:3000/api/items/fetch")
+      .then((res) => {
+        // console.log(res);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = nftdata.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(nftdata);
+    }
+  };
+  const [searchInput, setSearchInput] = useState("");
+
+  const [filteredResults, setFilteredResults] = useState([]);
 
   return (
     <div>
-     
-      <div >
-      {nftdata.map((elem, i) => (
-       <Card sx={{ maxWidth: 345 }}>
-       <CardActionArea>
-         <CardMedia
-           component="img"
-           height="140"
-           image={elem.img}
-           alt="green iguana"
-         />
-         <CardContent>
-           <Typography gutterBottom variant="h5" component="div">
-           {elem.name}
-           </Typography>
-           <Typography variant="body2" color="text.secondary">
-           {elem.price}
-           {elem.description}
-           </Typography>
-         </CardContent>
-       </CardActionArea>
-     </Card>
-      ))}
+      <input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => searchItems(e.target.value)}
+      />
+
+      <div>
+        {searchInput.length > 1
+          ? filteredResults.map((elem) => {
+              return (
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea>
+                  <img
+                  src={elem.upload}
+                  cloudname="dhgzyelo6"
+                  // public_id={elem.upload}
+                  alt="mouse corsair"
+                  class="mouse"
+                />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {elem.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {elem.price_bid}
+                        {elem.description}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              );
+            })
+          : nftdata.map((elem, i) => (
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                <img
+                  src={elem.upload}
+                  cloudname="dhgzyelo6"
+                  // public_id={elem.upload}
+                  alt="mouse corsair"
+                  class="mouse"
+                />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    {elem.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    {elem.price_bid}
+                        {elem.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
+      </div>
     </div>
-    </div>
-   
   );
 };
-
